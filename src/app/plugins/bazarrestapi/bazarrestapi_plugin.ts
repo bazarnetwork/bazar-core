8a85bc73662cfbc6e200d6c1d55428d7c8cdb8ea
Diego Cortés <108358550+diegocortes-cafeto@users.noreply.github.com>
2022-11-14 21:20:18 -0500

    Merge pull request #16 from bazarnetwork/feature/BNS-115-A
    BNS-115: Config deploy on AWS for Bazar testnet to the public

--- src/app/plugins/bazarrestapi/bazarrestapi_plugin.ts
+++ src/app/plugins/bazarrestapi/bazarrestapi_plugin.ts
@@ -1,12 +1,16 @@
-import { BasePlugin, PluginInfo } from 'lisk-sdk';
+import { apiClient, BasePlugin, PluginInfo } from 'lisk-sdk';
 import type { BaseChannel, EventsDefinition, ActionsDefinition, SchemaWithDefault } from 'lisk-sdk';
 import * as cors from 'cors';
 import * as express from 'express';
 import * as multer from 'multer';
 import { Server } from 'http';
 //import sendTransaction from './controller/transaction/sendTransaction';
+import getAccountById from './controller/account/getAccountById';
+import getForgers from './controller/forger/getForgers';
+import getConnectedPeers from './controller/peer/getConnectedPeers';
+import getLastBlock from './controller/block/getLastBlock';
+import getBlockById from './controller/block/getBlockById';
 import postFileController from './controller/files/postFileController';
-import path = require('path');
 
  /* eslint-disable class-methods-use-this */
  /* eslint-disable  @typescript-eslint/no-empty-function */
@@ -14,6 +18,9 @@ import path = require('path');
     private _app: express.Express | undefined = undefined;
     private _server: Server | undefined = undefined;
     private _channel: BaseChannel | undefined = undefined;
+    private _client: apiClient.APIClient | undefined = undefined;
+    
+    private ENDPOINT_PATH: String = '/api/v1';
 
 	public static get alias(): string {
 		return 'bazarrestapi';
@@ -47,26 +54,37 @@ import path = require('path');
 	}
 
 	public get actions(): ActionsDefinition {
-		return {
-		// 	hello: async () => { hello: 'world' },
-		};
+		return {};
 	}
 
 	public async load(channel: BaseChannel): Promise<void> {
         this._app = express();
-        this._channel = channel;
+        this._channel = channel;       
+        
+        const client = await this.getClient();
         const storage = multer.memoryStorage();
         const upload = multer({ storage: storage});
-
 
         this._app.use(cors({ origin: '*', methods: ['GET', 'POST', 'PUT']}));
         this._app.use(express.json())
         
-        this._app.post('/files/new', upload.single('file'), postFileController());
+        this._app.get(`${this.ENDPOINT_PATH}/account/:address`, getAccountById(this._channel, client));
+        this._app.get(`${this.ENDPOINT_PATH}/forgers`, getForgers(this._channel));
+        this._app.get(`${this.ENDPOINT_PATH}/peers/connected`, getConnectedPeers(this._channel));
+        this._app.get(`${this.ENDPOINT_PATH}/blocks`, getLastBlock(this._channel, client));
+        this._app.get(`${this.ENDPOINT_PATH}/blocks/:id`, getBlockById(this._channel, client));
+        this._app.post(`${this.ENDPOINT_PATH}/files/new`, upload.single('file'), postFileController());
 
 		this._server = this._app.listen(8088, '0.0.0.0');
 	}
 
 	public async unload(): Promise<void> {}
+
+    private async getClient() {
+        if (!this._client) {
+            this._client = await apiClient.createIPCClient('~/.lisk/bazar-core');
+        }
+        return this._client;
+    }
 }
 
