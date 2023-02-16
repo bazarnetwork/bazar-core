6608a0792cec11b5506956d3c46dec0354c3c724
Diego Cortés <dcortes@cafetosoftware.com>
2023-02-16 12:01:24 -0500

    BNS-344 : Configure a validator node
    Added changes for validator node.

--- src/app/plugins.ts
+++ src/app/plugins.ts
@@ -1,11 +1,9 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { DashboardPlugin } from "@liskhq/lisk-framework-dashboard-plugin";
-import { FaucetPlugin } from "@liskhq/lisk-framework-faucet-plugin";
 import { Application } from 'lisk-sdk';
 import { BazarrestapiPlugin } from "./plugins/bazarrestapi/bazarrestapi_plugin";
 
 export const registerPlugins = (app: Application): void => {
     app.registerPlugin(DashboardPlugin);
-    app.registerPlugin(FaucetPlugin);
     app.registerPlugin(BazarrestapiPlugin);
 };
