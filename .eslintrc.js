d0960a1f1dadf8674640eb1b526aff98ca10cfa7
C. Daniel Sanchez R <cdsanchezr@unal.edu.co>
2023-05-08 17:57:43 -0500

    fixing eslint bug.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,25 @@
 module.exports = {
 	root: true,
 	parserOptions: {
+		allowImportExportEverywhere: false,
 		project: './tsconfig.json',
+		sourceType: 'module',
 		tsconfigRootDir: __dirname,
 	},
 	extends: ['lisk-base/ts'],
+	env: {
+		// browser: true,
+		node: true,
+	},
+	rules: {
+		'import/no-extraneous-dependencies': [
+			'error',
+			{
+				'devDependencies': false,
+				'optionalDependencies': false,
+				'peerDependencies': false,
+				'packageDir': './'
+			},
+		],
+	},
 };
