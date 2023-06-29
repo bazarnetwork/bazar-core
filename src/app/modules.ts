ae0a080bff7599f628eecae5404a6845b5d3e888
C. Daniel Sanchez R <cdsanchezr@unal.edu.co>
2023-06-29 06:22:59 -0500

    Updating dependencies (non exhaustive).

--- src/app/modules.ts
+++ src/app/modules.ts
@@ -3,7 +3,6 @@ import { Application } from 'lisk-sdk';
 import { BuyerModule } from './modules/buyer/buyer_module';
 import { SellerModule } from './modules/seller/seller_module';
 
-// @ts-expect-error Unused variable error happens here until at least one module is registered
 export const registerModules = (app: Application): void => {
   app.registerModule(SellerModule);
   app.registerModule(BuyerModule);
