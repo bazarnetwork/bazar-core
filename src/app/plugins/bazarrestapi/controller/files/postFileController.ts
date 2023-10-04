e4296c48b9235caf9caeb57dab4297ebd01304b7
C. Daniel Sanchez R <cdsanchezr@unal.edu.co>
2023-10-04 13:33:22 -0500

    Fixing little things.

--- src/app/plugins/bazarrestapi/controller/files/postFileController.ts
+++ src/app/plugins/bazarrestapi/controller/files/postFileController.ts
@@ -4,12 +4,17 @@ import { Request, Response } from 'express';
 export default () => async (req: Request, res: Response) => {
   try {
     const ipfs = await create();
-    const { cid } = await ipfs.add({ path: req.file.originalname, content: req.file.buffer });
-    res.status(200).json({
-      data: { hash: cid.toString(), filename: req.file.originalname },
-      errorMessage: null,
-      errorCode: null,
-    });
+    const { file } = req;
+    if (file) {
+      const { cid } = await ipfs.add({ path: file.originalname, content: file.buffer });
+      res.status(200).json({
+        data: { hash: cid.toString(), filename: file.originalname },
+        errorMessage: null,
+        errorCode: null,
+      });
+    } else {
+      new Error('No File found.')
+    }
   } catch (err: unknown) {
     res.status(400).json({
       data: null,
